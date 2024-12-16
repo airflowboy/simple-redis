c179cb4fcc6828cde5120e32c384e9784d0c7d51
airflowboy <ajtwoddl9194@gmail.com>
2024-12-16 16:41:09 +0900

    simple redis 연습

---
+++ src/test/java/com/example/simple_redis/SimpleRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
